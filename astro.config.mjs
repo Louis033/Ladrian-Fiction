0eb899fcb438a8f23c1f97164433383dfc13281e
Louis033 <67913383+Louis033@users.noreply.github.com>
2026-01-07 00:34:59 +0700

    Fix: move Github action workflow to repo root

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,8 +37,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 const adapter = process.env.CF_PAGES ? cloudflarePages() : vercel({ mode: "serverless" });
 
 export default defineConfig({
-    site: siteConfig.siteURL,
-    base: "/",
+    site: "https://louis033.github.io",
+    base: "/Ladrian-Fiction",
     trailingSlash: "always",
     adapter: adapter,
     integrations: [
